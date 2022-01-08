6b9fabfea22d8fd133c7b877ac47deedbbe9288b
Florian Thiévent <hi@thievent.org>
2022-01-08 14:12:08 +0100

    init

---
+++ src/config.h
@@ -0,0 +1,11 @@
+const int FRONT_LEFT_SHOULDER = 0;
+const int FRONT_LEFT_KNEE = 1;
+
+const int FRONT_RIGHT_SHOULDER = 3;
+const int FRONT_RIGHT_KNEE = 4;
+
+const int BACK_LEFT_SHOULDER = 6;
+const int BACK_LEFT_KNEE = 7;
+
+const int BACK_RIGHT_SHOULDER = 9;
+const int BACK_RIGHT_KNEE = 10;
